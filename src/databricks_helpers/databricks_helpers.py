7d9a3296ebbf68b42c8c349fc552d86953ba44dd
Kelsey Mok <kelseymok@gmail.com>
2023-01-02 11:52:04 +0100

    using a class

--- src/databricks_helpers/databricks_helpers.py
+++ src/databricks_helpers/databricks_helpers.py
@@ -1,17 +1,13 @@
-# def get_dbutils(spark):
-#   from pyspark.dbutils import DBUtils
-#   return DBUtils(spark)
-#
-# def current_user() -> str:
-#     return
-#
-#
-def working_dir(current_user: str, exercise_name: str) -> str:
-    return f"/FileStore/{current_user}/last_connection_time_of_chargepoint"
+class DataDerpDatabricksHelpers:
+    def __init__(self, dbutils):
+        self.dbutils = dbutils
 
+    def current_user(self) -> str:
+        return self.dbutils.notebook.entry_point.getDbutils().notebook().getContext().userName().get().split("@")[0]
 
-def setup_notebook(exercise_name, dbutils_session):
-    current_user = dbutils_session.notebook.entry_point.getDbutils().notebook().getContext().userName().get().split("@")[0]
-    working_directory = working_dir(current_user, exercise_name)
-    dbutils_session.fs.rm(working_directory, True)
-    return current_user, working_directory
+    def working_directory(self, current_user: str, exercise_name: str) -> str:
+        return f"/FileStore/{current_user}/{exercise_name}"
+
+    def clean_notebook(self, working_dir: str) -> bool:
+        self.dbutils.fs.rm(working_dir, True)
+        return True
