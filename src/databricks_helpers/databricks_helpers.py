b97de3a8e23de1e021d8c7849ecd2604c455f6e7
Kelsey Mok <kelseymok@gmail.com>
2023-01-02 11:43:26 +0100

    using dbutils session to prevent clashing

--- src/databricks_helpers/databricks_helpers.py
+++ src/databricks_helpers/databricks_helpers.py
@@ -10,8 +10,8 @@ def working_dir(current_user: str, exercise_name: str) -> str:
     return f"/FileStore/{current_user}/last_connection_time_of_chargepoint"
 
 
-def setup_notebook(exercise_name: str, dbutils):
-    current_user = dbutils.notebook.entry_point.getDbutils().notebook().getContext().userName().get().split("@")[0]
+def setup_notebook(exercise_name: str, dbutils_session):
+    current_user = dbutils_session.notebook.entry_point.getDbutils().notebook().getContext().userName().get().split("@")[0]
     working_directory = working_dir(current_user, exercise_name)
-    dbutils.fs.rm(working_directory, True)
+    dbutils_session.fs.rm(working_directory, True)
     return current_user, working_directory
