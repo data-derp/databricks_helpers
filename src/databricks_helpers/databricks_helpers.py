57f3b258bf707234ed76f7de12342c254d0d39a4
Kelsey Mok <kelseymok@gmail.com>
2023-01-02 11:35:02 +0100

    using one function to get around dbutils issue

--- src/databricks_helpers/databricks_helpers.py
+++ src/databricks_helpers/databricks_helpers.py
@@ -1,11 +1,17 @@
-def current_user() -> str:
-    return dbutils.notebook.entry_point.getDbutils().notebook().getContext().userName().get().split("@")[0]
-
-
+# def get_dbutils(spark):
+#   from pyspark.dbutils import DBUtils
+#   return DBUtils(spark)
+#
+# def current_user() -> str:
+#     return
+#
+#
 def working_dir(current_user: str, exercise_name: str) -> str:
     return f"/FileStore/{current_user}/last_connection_time_of_chargepoint"
 
 
-def setup_notebook(exercise_name: str):
-    working_directory = working_dir(current_user(), exercise_name)
+def setup_notebook(exercise_name: str, dbutils):
+    current_user = dbutils.notebook.entry_point.getDbutils().notebook().getContext().userName().get().split("@")[0]
+    working_directory = working_dir(current_user, exercise_name)
     dbutils.fs.rm(working_directory, True)
+    return current_user, working_directory
